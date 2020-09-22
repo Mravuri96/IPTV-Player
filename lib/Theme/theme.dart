10dbc6f83b978af46f4ef315b9c44728a3f153f9
Mravuri96 <mravuri96@gmail.com>
2020-09-21 17:20:01 -0700

    removed google fonts

--- lib/Theme/theme.dart
+++ lib/Theme/theme.dart
@@ -1,14 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 ThemeData get dartkTheme => ThemeData.dark().copyWith(
-      textTheme: GoogleFonts.comfortaaTextTheme(
-        ThemeData(
-          primarySwatch: Colors.teal,
-          primaryColor: Colors.black,
-          brightness: Brightness.light,
-        ).textTheme,
-      ),
       elevatedButtonTheme: ElevatedButtonThemeData(
         style: ElevatedButton.styleFrom(
           primary: const Color(0xffFCCFA8),
